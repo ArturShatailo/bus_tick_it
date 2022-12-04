1859e25d2a8d3239c11461abcf596c768fc4cc98
Arthur <arturshatailo@gmail.com>
2022-12-04 20:01:34 +0200

    ticket service methods are logged and JavaDocs are created

--- tickets_management/src/main/java/com/tickets/tickets_managemet/controller/TicketsManagement.java
+++ tickets_management/src/main/java/com/tickets/tickets_managemet/controller/TicketsManagement.java
@@ -23,11 +23,10 @@ public interface TicketsManagement {
 
     /**
      * Checks status of payment of each Ticket and finds ones with "NEW" status. Filters them according
-     * to the status, time of the last check, deleted boolean and checked boolean. Sets new statuses for
+     * to the FilterService methods. Sets new statuses for
      * payments of filtered tickets randomly. Saves all the changes in database.
-     * In case of any ticket receives status "FAIL", the id of route as a key and amount of available tickets
-     * as a value will be added to the created map. Returns created map of routes ids and theirs
-     * available tickets amounts.
+     * Tickets with "FAIL" status will cause adding Route data into the created map
+     * that will be returned.
      *
      * @return map of id of routes (keys) and amount of available tickets on routes (values).
      */
--- tickets_management/src/main/java/com/tickets/tickets_managemet/service/tickets_status_processor/TicketProcessorService.java
+++ tickets_management/src/main/java/com/tickets/tickets_managemet/service/tickets_status_processor/TicketProcessorService.java
@@ -4,6 +4,18 @@ import java.util.Map;
 
 public interface TicketProcessorService {
 
+    /**
+     * Checks status of payment of each Ticket and finds ones with "NEW" status. Filters them according
+     * to the status, time of the last check, deleted boolean and checked boolean. Sets new statuses for
+     * payments of filtered tickets randomly. Saves all the changes in database.
+     * In case of any ticket receives status "FAIL", it receives checked marker and the id of route as a key and amount of available tickets
+     * as a value will be added to the created map. Returns created map of routes ids and theirs
+     * available tickets amounts.
+     * In case of any ticket receives status "DONE", it receives checked marker.
+     * Receiving status "NEW" will not provide any changes.
+     *
+     * @return map of id of routes (keys) and amount of available tickets on routes (values).
+     */
     Map<Long, Integer> ticketsCheck();
 
 }
